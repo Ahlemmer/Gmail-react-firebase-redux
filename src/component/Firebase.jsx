262ea48af9ee0ad4629e79eaec8cf2002c648fab
Ahlemmer <88335797+Ahlemmer@users.noreply.github.com>
2023-02-12 20:42:00 +0100

    Update Firebase.jsx

--- src/component/Firebase.jsx
+++ src/component/Firebase.jsx
@@ -4,12 +4,12 @@ import {getFirestore}from 'firebase/firestore'
 import{getAuth,GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfSCwaVL4Th2PqzDK1e6jCLfXDttEOi6Q",
-  authDomain: "clone-c7043.firebaseapp.com",
-  projectId: "clone-c7043",
-  storageBucket: "clone-c7043.appspot.com",
-  messagingSenderId: "434320969048",
-  appId: "1:434320969048:web:de44d1215c1caff83d292d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
